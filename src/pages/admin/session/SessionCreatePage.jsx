c9af2a8fbe63d5cb0a4a9ad1dc2b1615c5d90b48
Sveita <sveitast7@gmail.com>
2024-09-04 16:08:15 +0700

    Feature : {Update MUI to sessionCreate.jsx}

--- src/pages/admin/session/SessionCreatePage.jsx
+++ src/pages/admin/session/SessionCreatePage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
+import {
+  Box,
+  TextField,
+  MenuItem,
+  Button,
+  Stack,
+  Typography,
+  Divider,
+} from "@mui/material";
 
 function SessionCreatePage() {
   const [session, setSession] = useState({
@@ -40,80 +46,116 @@ function SessionCreatePage() {
 
   const period = [
     {
-      value: "",
+      value: "period1",
       label: "7:00 - 8:00",
     },
     {
-      value: "",
+      value: "period2",
       label: "8:10 - 9:00",
     },
     {
-      value: "",
+      value: "period3",
       label: "9:10 - 10:00",
     },
   ];
 
   const classes = [
     {
-      value: "",
+      value: "class1",
       label: "12A",
     },
     {
-      value: "",
+      value: "class2",
       label: "12B",
     },
     {
-      value: "",
+      value: "class3",
       label: "12C",
     },
   ];
 
   const subject = [
     {
-      value: "",
+      value: "math",
       label: "Math",
     },
     {
-      value: "",
+      value: "khmer",
       label: "Khmer",
     },
     {
-      value: "",
+      value: "english",
       label: "English",
     },
   ];
   const dow = [
     {
-      value: "",
+      value: "mon",
       label: "Monday",
     },
     {
-      value: "",
+      value: "tues",
       label: "Tuesday",
     },
     {
-      value: "",
+      value: "wed",
       label: "Wednesday",
     },
   ];
+
+  const form = {
+    maxWidth: "1064px",
+    padding: "32px",
+    border: "1px solid #eee",
+    borderRadius: "8px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "16px",
+  };
+
+  const container = {
+    "& .MuiTextField-root": { m: 1, width: 1 },
+    width: "100%",
+    display: "grid",
+    gap: 2 , 
+    gridTemplateColumns: 'repeat(2, 1fr)', 
+  };
+
+  const textInput = { width: 1 };
+
+  const buttonContainer = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: "16px",
+    width: 1,
+  };
   return (
-    <div>
-      <Box
-        component="form"
-        sx={{ "& .MuiTextField-root": { m: 1, width: "60ch" } }}
-        noValidate
-        autoComplete="off"
-      >
-        <Box sx={{ display: "flex", flexDirection: "row" }}>
+    <Box sx={{ maxWidth: 1200, mx: "auto", my: "32px", px: "32px" }}>
+      {/* Header */}
+      <Box sx={{ maxWidth: 1064, my: "32px" }}>
+        <Typography sx={{ fontSize: "32px", fontWeight: "semibold" }}>
+          ADD SESSION
+        </Typography>
+        <Typography sx={{ color: "#666666", fontSize: "16px" }}>
+          Please Fill Session information
+        </Typography>
+      </Box>
+
+      {/* Form */}
+      <Box sx={form}>
+        <Typography sx={{ fontSize: "18px", fontWeight: "bold" }}>
+          Session Information
+        </Typography>
+        <Divider sx={{ my: "16px" }} />
+        <Box component="form" sx={container} noValidate autoComplete="off" >
           {/* Teacher */}
-          <div>
-            <h4>Teacher</h4>
+          <Box sx={textInput}>
+            <Typography sx={{ fontSize: "16px" }}>Teacher</Typography>
             <TextField
               id="outlined-select-currency"
               select
-              label="Select"
-              defaultValue="EUR"
-              helperText="Please select your currency"
+              defaultValue="teacher1"
             >
               {teachers.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -121,16 +163,16 @@ function SessionCreatePage() {
                 </MenuItem>
               ))}
             </TextField>
-          </div>
+          </Box>
           {/* Period */}
-          <div>
-            <h4>Class Period</h4>
+          <Box sx={textInput}>
+            <Typography sx={{ fontSize: "16px", maxwidth: "524px" }}>
+              Class Period
+            </Typography>
             <TextField
               id="outlined-select-currency"
               select
-              label="Select"
-              defaultValue="EUR"
-              helperText="Please select your currency"
+              defaultValue="period1"
             >
               {period.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -138,19 +180,15 @@ function SessionCreatePage() {
                 </MenuItem>
               ))}
             </TextField>
-          </div>
-        </Box>
+          </Box>
 
-        <Box  sx={{ display: "flex", flexDirection: "row" }}>
           {/* Classes */}
-          <div>
-            <h4>Class</h4>
+          <Box sx={textInput}>
+            <Typography sx={{ fontSize: "16px" }}>Class</Typography>
             <TextField
               id="outlined-select-currency"
               select
-              label="Select"
-              defaultValue="EUR"
-              helperText="Please select your currency"
+              defaultValue={"class1"}
             >
               {classes.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -158,16 +196,14 @@ function SessionCreatePage() {
                 </MenuItem>
               ))}
             </TextField>
-          </div>
+          </Box>
           {/* Subject */}
-          <div>
-            <h4>Class</h4>
+          <Box sx={textInput}>
+            <Typography sx={{ fontSize: "16px" }}>Subject</Typography>
             <TextField
               id="outlined-select-currency"
               select
-              label="Select"
-              defaultValue="EUR"
-              helperText="Please select your currency"
+              defaultValue={"math"}
             >
               {subject.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -175,28 +211,32 @@ function SessionCreatePage() {
                 </MenuItem>
               ))}
             </TextField>
-          </div>
+          </Box>
+
+          {/* Dow */}
+          <Box sx={{ maxWidth: "524px" }}>
+            <Typography sx={{ fontSize: "16px" }}>Day of Week</Typography>
+            <TextField id="outlined-select-currency" select defaultValue="mon">
+              {dow.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Box>
         </Box>
 
-        {/* Dow */}
-        <div>
-          <h4>Day of Week</h4>
-          <TextField
-            id="outlined-select-currency"
-            select
-            label="Select"
-            defaultValue="EUR"
-            helperText="Please select your currency"
-          >
-            {dow.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        </div>
+        {/* Button  */}
+        <Stack sx={buttonContainer}>
+          <Button variant="outlined" sx={{ width: "170px" }} color="white">
+            Cancel
+          </Button>
+          <Button variant="contained" sx={{ width: "170px" }}>
+            Create
+          </Button>
+        </Stack>
       </Box>
-    </div>
+    </Box>
   );
 }
 
