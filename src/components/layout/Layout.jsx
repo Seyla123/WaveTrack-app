88446d2099c9f4e9a696b8a77db53a4af90671ff
NarakCODE <channarakluy@gmail.com>
2024-09-05 14:16:57 +0700

    Feature : Create sidebar link & icons

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,43 +1,86 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { AppProvider } from "@toolpad/core/AppProvider";
-import DescriptionIcon from "@mui/icons-material/Description";
-import FolderIcon from "@mui/icons-material/Folder";
+
 import theme from "../../styles/theme";
-import { Button } from "@mui/material";
+import {
+    Home as HomeIcon,
+    CalendarMonth as CalendarMonthIcon,
+    Class as ClassIcon,
+    Subject as SubjectIcon,
+    School as StudentIcon,
+    CoPresent as TeacherIcon,
+    LibraryBooks as ClassPeriodIcon,
+    Settings as SettingsIcon,
+    Logout as LogoutIcon,
+    Person,
+} from "@mui/icons-material";
+import ReportGmailerrorredIcon from "@mui/icons-material/ReportGmailerrorred";
+import ChecklistIcon from "@mui/icons-material/Checklist";
+import { Box } from "@mui/system";
 
 const navigation = [
+    { kind: "Menu", title: "Menu" },
     {
         segment: "dashboard",
         title: "Dashboard",
-        icon: <FolderIcon />,
+        icon: <HomeIcon />,
     },
     {
         segment: "session",
         title: "Sessions",
-        icon: <FolderIcon />,
+        icon: <CalendarMonthIcon />,
     },
     {
-        segment: "student",
-        title: "Students",
-        icon: <FolderIcon />,
-        children: [
-            {
-                segment: "lord-of-the-rings",
-                title: "Lord of the Rings",
-                icon: <DescriptionIcon />,
-            },
-            {
-                segment: "harry-potter",
-                title: "Harry Potter",
-                icon: <DescriptionIcon />,
-            },
-        ],
+        segment: "class",
+        title: "Class",
+        icon: <ClassIcon />,
     },
     {
         segment: "subject",
-        title: "Subjects",
-        icon: <FolderIcon />,
+        title: "Subject",
+        icon: <SubjectIcon />,
+    },
+    {
+        segment: "students",
+        title: "Students",
+        icon: <StudentIcon />,
+    },
+    {
+        segment: "teacher",
+        title: "Teacher",
+        icon: <TeacherIcon />,
+    },
+    {
+        segment: "class-periods",
+        title: "Class Periods",
+        icon: <ClassPeriodIcon />,
+    },
+    {
+        segment: "report",
+        title: "Report",
+        icon: <ReportGmailerrorredIcon />,
+        children: [
+            {
+                segment: "attendance",
+                title: "Attendance",
+                icon: <ChecklistIcon />,
+            },
+        ],
+    },
+    { kind: "Menu", title: "General" },
+
+    {
+        segment: "setting",
+        title: "Settings",
+        icon: <SettingsIcon />,
+        children: [
+            {
+                segment: "account",
+                title: "Account",
+                icon: <Person />,
+            },
+        ],
     },
 ];
 
@@ -79,14 +122,14 @@ const Layout = ({ children }) => {
     }, [location, navigate]);
     return (
         <AppProvider
+            branding={{ title: "WaveTrack" }}
             navigation={navigation}
             router={router}
             theme={theme}
             session={session}
             authentication={authentication}
         >
-            {children}
-            <Button>Chllo</Button>
+            <Box padding={2}>{children}</Box>
         </AppProvider>
     );
 };
