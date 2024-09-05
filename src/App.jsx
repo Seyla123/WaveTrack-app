bac69ace0af7611fc961a56c2505d2f1c933be7d
NarakCODE <channarakluy@gmail.com>
2024-09-05 11:40:24 +0700

    Feature : Config dashboard theme and add navigation data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import ProtectedRoutes from "./routes/ProtectedRoutes";
 import { Box } from "@mui/material";
 import { CssBaseline } from "@mui/material";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import SideBar from "./components/layout/SideBar";
 
 // Authentication Pages
 const LoginPage = lazy(() => import("./pages/auth/login/LoginPage"));
 const SignupPage = lazy(() => import("./pages/auth/signup/SignupPage"));
-import SideBar from "./components/layout/SideBar";
 
 // Protected Pages
 const DashboardPage = lazy(
@@ -60,41 +60,58 @@ const TeacherUpdatePage = lazy(() => import('./pages/admin/teacher/TeacherUpdate
 
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
-const AppRoutes = () => (
-    <>
-        <CssBaseline />
-        <Box>
-            <SideBar />
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
+const App = () => {
+    return (
+        // Toolpad library
+        <SideBar>
+            <CssBaseline />
+            <Box>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <DashboardLayout>
+                        <Routes>
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/signup" element={<SignupPage />} />
 
-                    {/* Protected Routes */}
-                    {/* <Route element={<ProtectedRoutes />}> */}
-                    <Route path="/dashboard" element={<DashboardPage />} />
+                            {/* Protected Routes */}
+                            {/* <Route element={<ProtectedRoutes />}> */}
+                            <Route
+                                path="/dashboard"
+                                element={<DashboardPage />}
+                            />
 
-                    {/* Session Routes */}
-                    <Route path="/session">
-                        <Route path="" element={<SessionListPage />} />
-                        <Route path=":id" element={<SessionDetailPage />} />
-                        <Route path="create" element={<SessionCreatePage />} />
-                        <Route
-                            path="update/:id"
-                            element={<SessionUpdatePage />}
-                        />
-                    </Route>
+                            {/* Session Routes */}
+                            <Route path="/session">
+                                <Route path="" element={<SessionListPage />} />
+                                <Route
+                                    path=":id"
+                                    element={<SessionDetailPage />}
+                                />
+                                <Route
+                                    path="create"
+                                    element={<SessionCreatePage />}
+                                />
+                                <Route
+                                    path="update/:id"
+                                    element={<SessionUpdatePage />}
+                                />
+                            </Route>
 
-                    {/* Student Routes */}
-                    <Route path="/student">
-                        <Route path="" element={<StudentListPage />} />
-                        <Route path=":id" element={<StudentDetailPage />} />
-                        <Route path="create" element={<StudentCreatePage />} />
-                        <Route
-                            path="update/:id"
-                            element={<StudentUpdatePage />}
-                        />
-                    </Route>
+                            {/* Student Routes */}
+                            <Route path="/student">
+                                <Route path="" element={<StudentListPage />} />
+                                <Route
+                                    path=":id"
+                                    element={<StudentDetailPage />}
+                                />
+                                <Route
+                                    path="create"
+                                    element={<StudentCreatePage />}
+                                />
+                                <Route
+                                    path="update/:id"
+                                    element={<StudentUpdatePage />}
+                                />
+                            </Route>
 
         {/* Subject */}
         <Route path="/subject">
@@ -121,11 +138,13 @@ const AppRoutes = () => (
       
       {/* </Route> */}
 
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-            </Suspense>
-        </Box>
-    </>
-);
+                            <Route path="*" element={<NotFoundPage />} />
+                        </Routes>
+                    </DashboardLayout>
+                </Suspense>
+            </Box>
+        </SideBar>
+    );
+};
 
-export default AppRoutes;
+export default App;
--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,42 +1,103 @@
 import React from "react";
 import { AppProvider } from "@toolpad/core/AppProvider";
-import { DashboardLayout } from "@toolpad/core/DashboardLayout";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import DescriptionIcon from "@mui/icons-material/Description";
-import LayersIcon from "@mui/icons-material/Layers";
+import {
+    Home as HomeIcon,
+    CalendarMonth as CalendarMonthIcon,
+    Class as ClassIcon,
+    Subject as SubjectIcon,
+    School as StudentIcon,
+    CoPresent as TeacherIcon,
+    LibraryBooks as ClassPeriodIcon,
+    Settings as SettingsIcon,
+    Logout as LogoutIcon,
+    ExpandMore as ExpandMoreIcon,
+} from "@mui/icons-material";
+import theme from "../../styles/theme";
 
-const SideBar = () => {
-    const NAVIGATION = [
-        { kind: "header", title: "Main items" },
-        { segment: "dashboard", title: "Dashboard", icon: <DashboardIcon /> },
-        { segment: "orders", title: "Orders", icon: <ShoppingCartIcon /> },
-        { kind: "divider" },
-        { kind: "header", title: "Analytics" },
-        {
-            segment: "reports",
-            title: "Reports",
-            icon: <BarChartIcon />,
-            children: [
-                { segment: "sales", title: "Sales", icon: <DescriptionIcon /> },
-                {
-                    segment: "traffic",
-                    title: "Traffic",
-                    icon: <DescriptionIcon />,
-                },
-            ],
-        },
-        {
-            segment: "integrations",
-            title: "Integrations",
-            icon: <LayersIcon />,
-        },
-    ];
+const NAVIGATION = [
 
+    {
+        kind: "header",
+        title: "Menu",
+    },
+    {
+        segment: "dashboard",
+        title: "Dashboard",
+        icon: <HomeIcon />,
+    },
+    {
+        segment: "session",
+        title: "Session",
+        icon: <CalendarMonthIcon />,
+    },
+    {
+        segment: "class",
+        title: "Class",
+        icon: <ClassIcon />,
+    },
+    {
+        segment: "subject",
+        title: "Subject",
+        icon: <SubjectIcon />,
+    },
+    {
+        segment: "students",
+        title: "Students",
+        icon: <StudentIcon />,
+    },
+    {
+        segment: "teacher",
+        title: "Teacher",
+        icon: <TeacherIcon />,
+    },
+    {
+        segment: "class-periods",
+        title: "Class Periods",
+        icon: <ClassPeriodIcon />,
+    },
+    {
+        kind: "header",
+        title: "Report",
+    },
+    {
+        segment: "attendance",
+        title: "Attendance",
+        icon: <ExpandMoreIcon />,
+    },
+    {
+        kind: "header",
+        title: "General",
+
+    },
+    {
+        segment: "settings",
+        title: "Settings",
+        icon: <SettingsIcon />,
+    },
+    {
+        segment: "account",
+        title: "Account",
+        icon: <ExpandMoreIcon />,
+    },
+    {
+        segment: "logout",
+        title: "Logout",
+        icon: <LogoutIcon />,
+    },
+];
+const SideBar = ({ children }) => {
     return (
-        <AppProvider navigation={NAVIGATION}>
-            <DashboardLayout />
+        <AppProvider
+            theme={theme}
+            navigation={NAVIGATION}
+            branding={{
+                logo: (
+                    <img src="https://mui.com/static/logo.png" alt="MUI logo" />
+                ),
+                title: "MUI",
+            }}
+        >
+            {children}
         </AppProvider>
     );
 };
