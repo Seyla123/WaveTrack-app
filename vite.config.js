06199de822eaf26fd970751ee1875fb58232bd6c
Seav Seyla <Mrrseyla.758@gmaill.com>
2024-09-11 11:49:07 +0700

    file : change vite file

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'], // Example: bundle React separately
-          // Add more chunks as needed
-        },
-      },
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
     },
-    chunkSizeWarningLimit: 1000, // Set a custom chunk size limit
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
 });
